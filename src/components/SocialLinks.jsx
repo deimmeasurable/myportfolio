2b365a87cc85ab5709fdc194b3400fa971ca54a7
Shola Azeez <sholaaz1805@gmail.com>
2022-07-23 11:48:22 +0000

    new

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -48,7 +48,7 @@ const SocialLinks = () => {
 
     ]
   return (
-    <div className='hidden lg:flex flex-col top-[75%] left-0 fixed'>
+    <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
 
             {links.map(({id, child, href, style, download}) =>(
